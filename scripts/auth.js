dff2d9f8947421d12e7169e94845ede870663b53
sinigers <sinigers@yahoo.com>
2021-06-09 14:54:32 +0300

    labs

--- scripts/auth.js
+++ scripts/auth.js
@@ -25,6 +25,24 @@ const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
   auth.signOut().then(() => {
-    console.log('user signed out');
+    console.log('user logout');
   })
 });
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) =>{
+  e.preventDefault();
+
+  //get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred =>{
+    console.log(cred.user);
+    //close the login modal and reset the form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  })
+})
