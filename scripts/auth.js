619f9fe770bf6792d2f45a49f4f811347dd7f768
sinigers <sinigers@yahoo.com>
2021-06-09 11:53:22 +0300

    labs

--- scripts/auth.js
+++ scripts/auth.js
@@ -18,3 +18,13 @@ signupForm.addEventListener('submit', (e) => {
     signupForm.reset();
   });
 });
+
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
+});
