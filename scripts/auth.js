349450085b58c37b9910c7637b088478bcb1741c
sinigers <sinigers@yahoo.com>
2021-06-09 15:05:56 +0300

    labs

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,14 @@
+//listen for auth status changes. to know if user is login or out
+auth.onAuthStateChanged(user =>{
+  console.log(user);
+  if(user){
+    console.log('user logged in:', user);
+  }else{
+    console.log('user logged out');
+  }
+});
+
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -24,9 +35,7 @@ signupForm.addEventListener('submit', (e) => {
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
-  auth.signOut().then(() => {
-    console.log('user logout');
-  })
+  auth.signOut()
 });
 
 //login
@@ -39,7 +48,7 @@ loginForm.addEventListener('submit', (e) =>{
   const password = loginForm['login-password'].value;
 
   auth.signInWithEmailAndPassword(email, password).then(cred =>{
-    console.log(cred.user);
+    // console.log(cred.user);
     //close the login modal and reset the form
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
