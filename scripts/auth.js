a1c5027b3425fb19fe412c26ba2211f90c513999
sinigers <sinigers@yahoo.com>
2021-06-11 17:08:54 +0300

    labs

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,4 +1,15 @@
 
+//add admin cloud function
+const adminForm =  document.querySelector('.admin-actions');
+adminForm.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  const adminEmail = document.querySelector('#admin-email').value;
+  const addAdminRole = functions.httpsCallable('addAdminRole');
+  addAdminRole({ email: adminEmail }).then(result =>{
+    console.log(result);
+  })
+})
+
 
 //listen for auth status changes. to know if user is login or out
 auth.onAuthStateChanged(user => {
